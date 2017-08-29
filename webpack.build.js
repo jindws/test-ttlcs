bc881623abddfc0af9b3fa87060e29e476bc217a
金大伟 <dawei.jin@xiaobao100.com>
2017-08-29 16:53:22 +0800

    yarn build

---
+++ webpack.build.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+        common: [
+            "jquery",
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+        chunkFilename: '[name].[hash].js',
+        libraryTarget:"umd",
+    },
+    resolve: {
+        extensions:[".js",".vue"],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.resolve(__dirname, 'components'),
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        },{
+            test: /\.css$/,
+            loader: 'style-loader!css-loader!postcss-loader',
+        },{
+          test: /\.(png|jpg)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: 'img/[name].[hash:7].[ext]'
+              }
+            }
+          ]
+        },{
+          test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+          loader: 'file-loader',
+          query: {
+              name: '[name].[ext]?[hash]',
+              publicPath:'../'
+          }
+        }]
+    },
+    watch:true,
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.ProvidePlugin({"$": "jquery"}),
+        new webpack.optimize.CommonsChunkPlugin({names: ['common'], minChunks: Infinity}),
+        new HtmlWebpackPlugin({template: './index.html'}),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+    ]
+}
