75fc75de9fd94fddc50c0bc51df75a30cc6def39
wujy <1738908366@qq.com>
2017-08-30 14:44:18 +0800

    代码格式。空四格。

--- pages/Index/Index.vue
+++ pages/Index/Index.vue
@@ -1,35 +1,35 @@
 <template>
-<section class='index'>
-    <XHeader/>
-    <section>
-        <LeftMenu/>
-        <XMain/>
+    <section class='index'>
+        <XHeader/>
+        <section>
+            <LeftMenu/>
+            <XMain/>
+        </section>
     </section>
-</section>
 </template>
 
 <script>
-import XHeader from '@/Header'
-import LeftMenu from '@/LeftMenu'
+    import XHeader from '@/Header'
+    import LeftMenu from '@/LeftMenu'
 
-import XMain from '../Main'
+    import XMain from '../Main'
 
-import { mapGetters } from 'vuex'
+    import { mapGetters } from 'vuex'
 
-export default {
-  mounted(){
-    this.$DB.Test.list({
-      name:1,
-    }).then(re=>{
-      console.log('成功',re)
-    },data=>{
-      console.log('失败',data)
-    })
-  },
-  components:{
-    XHeader,
-    LeftMenu,
-    XMain
-  }
-}
+    export default {
+        mounted(){
+            this.$DB.Test.list({
+                name:1,
+            }).then(re=>{
+                console.log('成功',re)
+            },data=>{
+                console.log('失败',data)
+            })
+        },
+        components:{
+            XHeader,
+            LeftMenu,
+            XMain
+        }
+    }
 </script>
