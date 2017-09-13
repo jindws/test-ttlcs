96161e48dbc4d7c42da8406495281fdc49572eaa
wujy <1738908366@qq.com>
2017-09-13 19:04:22 +0800

    问题

--- pages/Html/IpWhiteListController/IpWhiteListController.vue
+++ pages/Html/IpWhiteListController/IpWhiteListController.vue
@@ -12,7 +12,7 @@
             <el-table-column prop="createName" label="创建者"></el-table-column>
             <el-table-column prop="updateName" label="修改者"></el-table-column>
             <el-table-column prop="status" label="状态"></el-table-column>
-            <el-table-column prop="operates" label="操作">
+            <el-table-column prop="operates" label="操作" width="200">
                 <template scope="scope">
                     <el-button type="danger" @click="deleted(scope.$index, scope.row)"
                                size="small" :disabled="deleteDisabled">删除
@@ -31,9 +31,9 @@
                        :current-page.sync="currentPage"
                        layout="prev, pager, next, jumper" :total="total" style="float:right;margin:20px 10px 0;">
         </el-pagination>
-        <!--TODO 新增界面-->
+        <!--新增界面-->
         <el-dialog title="添加公共ip白名单" :visible.sync="addIpWhiteListVisible" :close-on-click-modal="false"
-                   @close="closeIpWhiteList('addIpWhiteListForm')">
+                   @close="IpWhiteListCloseReset('addIpWhiteListForm')">
             <el-form :model="addIpWhiteListForm" :rules="addIpWhiteListRules" ref="addIpWhiteListForm">
                 <el-form-item label="公共白名单ip" :label-width="formLabelWidth" prop="ip">
                     <el-input v-model="addIpWhiteListForm.ip" auto-complete="off"
@@ -41,17 +41,46 @@
                 </el-form-item>
             </el-form>
             <div slot="footer" class="dialog-footer">
-                <el-button @click="IpWhiteListReset">重置</el-button>
+                <el-button @click="IpWhiteListCloseReset('addIpWhiteListForm')">重置</el-button>
                 <el-button type="success" @click="addIpWhiteListpSub">立即提交</el-button>
             </div>
         </el-dialog>
-        <!--TODO 编辑界面-->
+        <!-- 编辑界面-->
+        <el-dialog title="编辑公共ip白名单" :visible.sync="editIpWhiteListVisible" :close-on-click-modal="false"
+                   @close="IpWhiteListCloseReset('editIpWhiteListForm')">
+            <el-form :model="editIpWhiteListForm" :rules="editIpWhiteListRules" ref="editIpWhiteListForm">
+                <el-form-item label="公共白名单ip" :label-width="formLabelWidth" prop="ip">
+                    <el-input v-model="editIpWhiteListForm.ip" auto-complete="off"
+                              placeholder="请输入公共白名单ip"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="editIpWhiteListCloseReset">重置</el-button>
+                <el-button type="success" @click="editIpWhiteListpSub">立即提交</el-button>
+            </div>
+        </el-dialog>
         <!--TODO 审批界面-->
+        <el-dialog title="审批" :visible.sync="approveIpWhiteListVisible" :close-on-click-modal="false"
+                   @close="approveIpWhiteListCloseReset">
+            <el-form :model="approveIpWhiteList" ref="approveIpWhiteList" style="margin-bottom: -24px;">
+                <el-form-item label="是否同意">
+                    <el-radio-group v-model="approveIpWhiteList.radio">
+                        <el-radio class="radio" label="AGREE" style="margin-left:20px">
+                            同意</el-radio>
+                        <el-radio class="radio" label="DISAGREE">不同意</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="success" @click="approveIpWhiteListpSub">立即提交</el-button>
+            </div>
+        </el-dialog>
     </section>
 </template>
 
 <script>
     import moment from 'moment'
+
     export default {
         data() {
             return {
@@ -70,8 +99,19 @@
                 deleteDisabled: true,
                 /*编辑*/
                 modifyDisabled: true,
+                editIpWhiteListVisible: false,
+                editIpWhiteListForm: {
+                    ip: ''
+                },
+                editIpWhiteListRules: {
+                    ip: [{required: true, message: '公共白名单ip不能为空', trigger: 'blur'}]
+                },
                 /*审批*/
                 approveDisabled: true,
+                approveIpWhiteListVisible: false,
+                approveIpWhiteList: {
+                    radio: ''
+                },
                 /*分页*/
                 currentPage: 1,
                 total: 0,
@@ -82,7 +122,7 @@
         },
         methods: {
             /*列表*/
-            getList(){
+            getList() {
                 this.$DB.IpWhiteList.list({
                     pagesize: '10',
                     pageNum: this.pageNum,
@@ -100,54 +140,141 @@
                     if (result.operates.includes("审批")) {
                         this.approveDisabled = false;
                     }
-                     /*工具栏，分页*/
-                     this.total = result.total;
-                     /!*列表展示，时间格式转换 *!/
-                     this.IpWhiteList = result.pageList.map(item => {
-                         return Object.assign({}, item, {
-                             startTime: moment(item.startTime).format('YYYY-MM-DD HH:mm:ss'),
-                             endTime: moment(item.endTime).format('YYYY-MM-DD HH:mm:ss')
-                         });
-                     });
-                },data => {
-                    console.log('失败',data);
-                })
-
+                    /*工具栏，分页*/
+                    this.total = result.total;
+                    /!*列表展示，时间格式转换 *!/
+                    this.IpWhiteList = result.pageList.map(item => {
+                        console.log(item.type)
+                        return Object.assign({}, item, {
+                            startTime: moment(item.startTime).format('YYYY-MM-DD HH:mm:ss'),
+                            endTime: moment(item.endTime).format('YYYY-MM-DD HH:mm:ss')
+                        });
+                    });
+                }, data => {
+                    console.log('失败', data);
+                });
             },
-            /*TODO 新增公共ip白名单*/
-            addIpWhiteList(){
+            /*新增公共ip白名单对话框*/
+            addIpWhiteList() {
                 this.addIpWhiteListVisible = true;
             },
-            IpWhiteListReset(){
-
+            /*关闭对话框，重置对话框*/
+            IpWhiteListCloseReset(formName) {
+                this.$refs[formName].resetFields();
             },
-            addIpWhiteListpSub(){
-
+            /*新增公共白名单ip提交*/
+            addIpWhiteListpSub() {
+                this.$refs.addIpWhiteListForm.validate((valid) => {
+                    if (valid) {
+                        this.$DB.IpWhiteList.add({
+                            ip: this.addIpWhiteListForm.ip
+                        }).then(result => {
+                            this.getList();
+                            this.addIpWhiteListVisible = false;
+                            this.$message({
+                                message: '添加成功',
+                                type: 'success'
+                            });
+                        }, data => {
+                            if (data.code == 3304) {
+                                window.location.href = '#/login';
+                            }
+                            console.log('失败', data)
+                        })
+                    }
+                });
             },
-            /*TODO 关闭新增对话框*/
-            closeIpWhiteList(){
-
+            /* 删除*/
+            deleted(index, row) {
+                this.$confirm('确定做删除操作吗?', '提示信息', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.$DB.IpWhiteList.deleted({
+                        id: row.id
+                    }).then(result => {
+                        this.getList();
+                        this.$message({
+                            type: 'success',
+                            message: '删除成功!'
+                        });
+                    }, data => {
+                        if (data.code == 3304) {
+                            window.location.href = '#/login';
+                        }
+                        console.log('失败', data);
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
             },
-            /*TODO 删除*/
-            deleted(index,row){
-
+            /* 编辑对话框*/
+            modify(index, row) {
+                this.editIpWhiteListVisible = true;
+                this.editIpWhiteListForm.ip = row.ip;
+                this.editIpWhiteListForm.id = row.id;
             },
-            /*TODO 编辑*/
-            modify(index,row){
-
+            /* 编辑重置*/
+            editIpWhiteListCloseReset() {
+                this.editIpWhiteListForm = {ip: ''};
+                this.$refs.editIpWhiteListForm.resetFields();
+            },
+            /* 编辑提交*/
+            editIpWhiteListpSub() {
+                this.$refs.editIpWhiteListForm.validate((valid) => {
+                    if (valid) {
+                        this.$DB.IpWhiteList.modify({
+                            id: this.editIpWhiteListForm.id,
+                            ip: this.editIpWhiteListForm.ip
+                        }).then(result => {
+                            this.editIpWhiteListVisible = false;
+                            this.getList();
+                            this.$message({
+                                type: 'success',
+                                message: '修改成功'
+                            });
+                        },data => {
+                            console.log('失败',data)
+                        })
+                    }
+                })
             },
             /*TODO 审批*/
-            approve(index,row){
-
+            approve(index, row) {
+                this.approveIpWhiteListVisible = true;
+                this.approveIpWhiteList.id = row.id;
             },
-            /*分页*/
+            /*TODO 审批成功*/
+            approveIpWhiteListpSub(){
+                this.$DB.IpWhiteList.approve({
+                    id: this.approveIpWhiteList.id,
+                    opinion: this.approveIpWhiteList.radio
+                }).then(result => {
+                    this.getList();
+                    this.approveIpWhiteListVisible = false;
+                    this.$message({
+                        type: 'success',
+                        message: '审批成功'
+                    });
+                },data => {
+                    console.log('失败',data)
+                });
+            },
+            /*TODO 审批关闭，重置界面*/
+            approveIpWhiteListCloseReset(){
+                this.approveIpWhiteList.radio = '';
+            },
+            /*分页跳转到输入的页面*/
             handleCurrentChange(val){
-                /*当前第几页*/
                 this.pageNum = val;
                 this.getList();
-            }
+            },
         },
-        mounted()  {
+        mounted() {
             /*列表*/
             this.getList();
         }
