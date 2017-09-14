9052d0f85abb860bce7ace6b1b4defed273e6aef
金大伟 <dawei.jin@xiaobao100.com>
2017-09-14 10:52:51 +0800

    bug fixed

--- pages/Html/OperateLogController/OperateLogController.vue
+++ pages/Html/OperateLogController/OperateLogController.vue
@@ -6,7 +6,7 @@
                 <el-input v-model="OperateLogControllerForm.user" placeholder="操作人"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-date-picker v-model="" type="datetime" placeholder="开始时间"></el-date-picker>
+                <el-date-picker type="datetime" placeholder="开始时间"></el-date-picker>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="queryOperateLogController">查询</el-button>
